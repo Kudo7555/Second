50d1f44476583479ff48b7ade346eece2d6c7f22
Kudo7555 <k63510736@gmail.com>
2025-04-04 16:09:43 +0630

    p and n.cpp

---
+++ positive and negative.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	
+	cout<<"enter a number=";
+	cin>>num;
+
+	if(num>0)
+	cout<<num<<" is positive num";
+	
+	else if(num<0)
+	cout<<num<<" is negative num";
+	
+	else if(num==0)
+	cout<<num<<" is equal to zero";
+	
+	
+	
+	return 0;
+}
